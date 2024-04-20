05bfbd73ed80600425fe8753157ec0f0cfe66356
francis-lp-win <franzdiaz460@gmail.com>
2024-04-20 13:19:58 +0400

    ref: able to get the relevant query

--- src/services/scraper.ts
+++ src/services/scraper.ts
@@ -1,4 +1,4 @@
-import { By, WebElement, until } from "selenium-webdriver";
+import { By, WebDriver, WebElement, until } from "selenium-webdriver";
 import { CustomWebDriver } from "../utils/chrome_driver";
 import { error } from "console";
 import { user_query } from "../types/user_query";
@@ -12,26 +12,31 @@ export class Scraper {
     let chrome = await this.driver.get_driver();
     try {
       if (chrome === null) throw error;
+      console.log(chrome?.getSession());
       await chrome.get(websiteURL);
       console.log(await chrome.getTitle());
-      const raw_data = await this.get_raw_query_data(dataQuery);
+      const raw_data = await this.get_raw_query_data(chrome, dataQuery);
       const populated_query = await this.get_data(
         raw_data as Array<WebElement[]>,
         dataQuery
       );
-      console.log(raw_data);
+      console.log(populated_query);
     } catch (error) {
       console.error("Driver does not exist.");
+      console.error(error);
     }
   }
 
-  private async get_raw_query_data(dataQuery: { [key: string]: any }) {
-    const chrome = await this.driver.get_driver();
-    if (!chrome) return;
-    const query_keys = Object.keys(dataQuery);
-    const map_query = query_keys.map(async (query) => {
+  private async get_raw_query_data(
+    chrome: WebDriver,
+    dataQuery: { [key: string]: any }
+  ) {
+    if (chrome === null) return;
+    console.log(chrome?.getSession());
+    const query_values = Object.values(dataQuery);
+    const map_query = query_values.map(async (query) => {
       try {
-        const g = await chrome.findElements(By.css(dataQuery[query]));
+        const g = await chrome.findElements(By.css(query));
         return g;
       } catch (e) {
         throw error(e);
@@ -46,16 +51,16 @@ export class Scraper {
     data_query: { [key: string]: any }
   ) {
     const query_keys = Object.keys(data_query);
-    console.log(raw_data[0]);
     const populate_query = raw_data[0].map(async (_, i) => {
       let populated_query: { [key: string]: any } = {};
       for (let j = 0; j < query_keys.length; j++) {
-        populated_query[query_keys[j]] = await raw_data[j][i].getText();
+        const query_item = raw_data[j][i];
+        if (query_item === undefined) break;
+        populated_query[query_keys[j]] = await query_item.getText();
       }
       return populated_query;
     });
     const resolve_text_data = await Promise.all(populate_query);
-    console.log(resolve_text_data);
-    return resolve_text_data;
+    return resolve_text_data.filter((item) => Object.values(item)[0] !== "");
   }
 }
