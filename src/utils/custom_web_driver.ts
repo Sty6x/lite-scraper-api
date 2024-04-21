6b721244dc9aecae80ecfcb801a5d6c8d264528a
francis-lp-win <franzdiaz460@gmail.com>
2024-04-21 11:56:05 +0400

    ref: adding a retry search function for polling

--- src/utils/custom_web_driver.ts
+++ src/utils/custom_web_driver.ts
@@ -13,6 +13,9 @@ export class CustomWebDriver {
 
   private async driver_initializer(): Promise<WebDriver | null> {
     const chromeOption = new chrome.Options();
+    const user_agent =
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+    chromeOption.setPageLoadStrategy("eager");
     chromeOption.addArguments("--headless=new");
     try {
       let driver = await new Builder()
