4757d63b9724d84a256806e45bacc79a9587c4b2
francis-lp-win <franzdiaz460@gmail.com>
2024-04-22 19:26:59 +0400

    ref: able to scrape contents from user's data query

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,8 @@ import chromeDriver from "../utils/custom_web_driver";
 
 const router = express.Router();
 router.post("/scrape", parser, (req: Request, res: Response) => {
-  const scraper = new Scraper(chromeDriver);
-  scraper.scrape(req.body.parsedData);
+  const scraper = new Scraper(req.body.parsedData);
+  scraper.scrape();
   console.log(req.body.parsedData);
   res.json(req.body.parsedData);
 });
--- src/services/scraper.ts
+++ src/services/scraper.ts
@@ -1,25 +1,55 @@
 import { CustomWebDriver } from "../utils/custom_web_driver";
-import { chromium } from "playwright";
+import { ChromiumBrowser, Locator, Page, chromium } from "playwright";
 import { error } from "console";
 import { user_query } from "../types/user_query";
 
 export class Scraper {
-  private driver: CustomWebDriver;
-  constructor(custom_driver: CustomWebDriver) {
-    this.driver = custom_driver;
+  query: user_query;
+  constructor(user_query: user_query) {
+    this.query = user_query;
   }
-  async scrape({ websiteURL, dataQuery }: user_query): Promise<void> {
+  async scrape(): Promise<void> {
+    const browser = await chromium.launch();
     try {
       const user_agent =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
-      const browser = await chromium.launch();
       const page = await browser.newPage({ userAgent: user_agent });
-      await page.goto(websiteURL);
-      await page.screenshot({ path: "here.png" });
-      await page.close();
+      await page.goto(this.query.websiteURL);
+      const raw_data = await this.get_raw_data(page);
+      const populated_query = await this.get_data(raw_data);
+      console.log(populated_query);
     } catch (error) {
       console.error("Something went wrong.");
       console.error(error);
+      await browser.close();
     }
+  }
+
+  private async get_raw_data(page: Page) {
+    const query_values = Object.values(this.query.dataQuery);
+    const map_query = query_values.map(async (query) => {
+      try {
+        const g = await page.locator(query).all();
+        return g;
+      } catch (e) {
+        throw error(e);
+      }
+    });
+    const raw_data = await Promise.all(map_query);
+    return raw_data;
+  }
+  private async get_data(raw_data: Array<Locator[]>) {
+    const query_keys = Object.keys(this.query.dataQuery);
+    const populate_query = raw_data[0].map(async (_, i) => {
+      let populated_query: { [key: string]: any } = {};
+      for (let j = 0; j < query_keys.length; j++) {
+        const query_item = raw_data[j][i];
+        if (query_item === undefined) break;
+        populated_query[query_keys[j]] = await query_item.textContent();
+      }
+      return populated_query;
+    });
+    const resolve_text_data = await Promise.all(populate_query);
+    return resolve_text_data;
   }
 }
