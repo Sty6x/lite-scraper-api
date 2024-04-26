1ede9510c42ce6a600a1d701f532f36cc5e74eca
francis-lp-win <franzdiaz460@gmail.com>
2024-04-26 12:59:33 +0400

    ref: handling if user's session has expired dummy validattion

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -21,14 +21,12 @@ async function auth(req: Request, res: Response, next: NextFunction) {
   if (eval_existing_user_credentials === undefined) {
     res.send(`Create an account before you can access this site.`);
   } else {
-    // this checks for session id  expiry THIS IS A DUMMY
     const eval_existing_user_id = users_database.users.find(
       (user) => user.session_id === user_session_id,
     );
     const timed_out = undefined;
-    // this checks for session id  expiry THIS IS A DUMMY
     if (
-      req.headers.cookie === timed_out || // if user exists but cookie is empty or there is not cookie session id
+      req.headers.cookie === undefined || // if user exists but cookie is empty or there is not cookie session id
       eval_existing_user_id === timed_out // if user exists but session id is expired create new session id
     ) {
       const existing_user = eval_existing_user_credentials;
@@ -40,22 +38,14 @@ async function auth(req: Request, res: Response, next: NextFunction) {
         ],
       };
       await writeFile("./users.json", JSON.stringify(update_database));
+      console.log(users_database);
       res.send(`Welcome Back ${existing_user.name}`);
-      return;
+    } else {
+      res.send(
+        `Welcome back ${eval_existing_user_credentials.name}\n
+       Your session id is: ${eval_existing_user_credentials.session_id}`,
+      );
     }
-    console.log(users_database);
-    res.send(
-      `Welcome back ${eval_existing_user_credentials.name}\n
-        Your session id is: ${eval_existing_user_credentials.session_id}`,
-    );
   }
 }
 export { auth };
-
-// should be handled in login route
-// const set_new_user = {
-//   id: req.session.id,
-//   name: user_name,
-//   cookie: { expire: req.session.cookie.maxAge },
-// };
-// User_Store.user_store.set(req.session.id, set_new_user);
